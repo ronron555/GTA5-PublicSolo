1c635e22f271d2e66b3048d0ba8bdd33e2508c8f
Ron Yitzhaki <ronyitzhaki5@gmail.com>
2021-01-08 18:35:52 +0200

    Added spaces in code

--- src/Public-Solo.bat
+++ src/Public-Solo.bat
@@ -1,15 +1,20 @@
 @echo off
 setlocal
+
 cd %UserProfile%\Scripts\PSTools
 cls
+
 echo Suspending GTA5.exe process...
 pssuspend.exe GTA5.exe > nul
 cls
+
 echo GTA5.exe process paused...
 echo Waiting 10 seconds...
 ping 127.0.0.1 -n 11 > nul
 cls
+
 echo Resuming GTA5.exe process..
 pssuspend.exe -r GTA5.exe > nul
+
 cd  %UserProfile%\Scripts
 call sendkeys.bat "Grand Theft Auto V" ""
